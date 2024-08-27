cc27f1850dc2794314f038f78d8f86f9f2b47b76
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-08-27 14:02:11 -0600

    Add inference script.

--- scripts/inference/scaling_results-swot-1.1v_20-20e.py
+++ scripts/inference/scaling_results-swot-1.1v_20-20e.py
@@ -9,9 +9,9 @@ eval_losses: Dict[str,Dict[str,float]]
 
 cname: str = "sres"
 model: str = 'rcan-10-20-64'
-nts: int = 2
+nts: int = 10
 varname = "SST"
-downsample_values = [ 1.1, 1.2] # 1.3, 1.5, 2.0, 2.5, 3.0 ]
+downsample_values = [ 1.1, 1.3, 1.5, 2.0, 2.5, 3.0 ]
 data_structure: ResultStructure = ResultStructure.Image
 
 configuration = dict(
@@ -39,7 +39,7 @@ for data_downsample in downsample_values:
 		downsample_results[data_downsample] =  mean_loss_pct
 
 print( f"\n Scaling Results for {configuration['dataset']}:{configuration['task']}")
-print( f" downsample_factor  mean_loss_pct ")
+print( f" downsample_factor vs mean_loss_pct ")
 for data_downsample, mean_loss_pct in downsample_results.items():
 	print( f" {data_downsample:.2f}   {mean_loss_pct:.2f}")
 
