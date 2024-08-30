99d5d20518f13634a5b41d9499766de3da80cf2f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-08-30 12:36:21 -0600

    Add inference script.

--- sres/data/tiles.py
+++ sres/data/tiles.py
@@ -23,7 +23,7 @@ class TileIterator(object):
         self.batch_losses(ltype).append( loss )
 
     def accumulate_loss(self, ltype: str):
-        print( f"accumulate_loss({ltype}), types = {list(self._batch_losses.keys())}" )
+        print( f"accumulate_loss({ltype}), losses = {self._batch_losses}" )
         accum_loss = np.array(self.batch_losses(ltype)).mean()
         self.clear_batch_losses(ltype)
         return accum_loss
