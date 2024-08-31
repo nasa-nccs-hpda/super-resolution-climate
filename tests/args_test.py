ebdb2ea0c899c1f91417ecccec8a2b88ea38210d
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-08-31 15:37:38 -0600

    Add inference script.

--- tests/args_test.py
+++ tests/args_test.py
@@ -3,4 +3,5 @@ import argparse
 cname = "sres"
 argparser = argparse.ArgumentParser(description=f'Execute workflow {cname}')
 argparser.add_argument('-r', '--refresh', action='store_true')
-print(argparser.parse_args())
+args: argparse.Namespace = argparser.parse_args()
+print( "Refresh = " + str(args.refresh))
